9e79d4d3428406aa100bb8cac423fe9e47c0da03
Leeiazz <leeiaz7554@gmail.com>
2024-12-13 09:05:59 -0300

    function verify palindrome finished

--- back/src/helpers/verify-palindrome.js
+++ back/src/helpers/verify-palindrome.js
@@ -1,1 +1,14 @@
-export const verifyPalindrome = (word = '') => {};
+export const verifyPalindrome = (word = '') => {
+  // Le saco los ascentos a las palabras en el caso de tener
+  const withoutAccent = word
+    .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase();
+  // Le saco los espacios para poder comparar también con su versión inversa
+  const withoutSpaces = withoutAccent.split(' ').join('');
+  // Genero la version inversa, por ej: casa => asac
+  const reverse = withoutSpaces.split('').reverse().join('');
+  // comparo ya si es palindromo o no
+  return withoutSpaces === reverse;
+};
--- back/src/index.js
+++ back/src/index.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { json } from 'express';
+import { verifyPalindrome } from './helpers/verify-palindrome.js';
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,7 @@ app.get('/ping', async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 3000));
   res.send({ message: 'pong' });
 });
+
 app.get('/historical', async (req, res) => {
   try {
     const registers = [];
@@ -33,8 +35,20 @@ app.get('/historical', async (req, res) => {
 });
 
 app.post('/verify-palindrome', async (req, res) => {
-  const { body } = req;
-  res.send({ message: `Palindromo: ${body}` });
+  try {
+    const { text } = req.body;
+    if (!text || text.length === 0)
+      return res.status(400).send({ message: 'Text is required' });
+
+    if (verifyPalindrome(text)) {
+      res.send({ message: `SI es palindromo: ${text}` });
+    } else {
+      res.send({ message: `NO es palindromo: ${text}` });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ registers: [], message: error.message });
+  }
 });
 
 app.listen(3000, () => {
