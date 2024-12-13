f4de6245c1abaf82f2e018193809364dbf76a8c8
Leeiazz <leeiaz7554@gmail.com>
2024-12-12 21:09:19 -0300

    /historical and /verify-palindrome endpoins added

--- back/src/index.js
+++ back/src/index.js
@@ -10,8 +10,31 @@ app.use(json());
  */
 
 // GET /
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get('/ping', async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  res.send({ message: 'pong' });
+});
+app.get('/historical', async (req, res) => {
+  try {
+    const registers = [];
+    for (let i = 0; i < 5; i++) {
+      registers.push({
+        text: `text ${i}`,
+        isPalindrome: true,
+        date: new Date().toISOString(),
+      });
+    }
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    res.send({ registers });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ registers: [], message: error.message });
+  }
+});
+
+app.post('/verify-palindrome', async (req, res) => {
+  const { body } = req;
+  res.send({ message: `Palindromo: ${body}` });
 });
 
 app.listen(3000, () => {
